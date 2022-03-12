2b3bf3311cdbadcc1c8e687a6c3744d821fd0dc9
jayden <jaydensterba@gmail.com>
2022-03-12 13:25:07 -0600

    yay

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,15 +6,16 @@ public class Main extends MagicSquareCreator {
 
     public static void main(String[] args) {
 
-        Scanner in = new Scanner(System.in);
         System.out.println("n = number of rows and columns (Ex. n = 2;  Matrix = 2x2)");
         System.out.println("m = magic number (number that all numbers in each row/column add up too)" + "\n");
-        System.out.print("Enter Integer Value for 'n' :: ");
         int n = 0;
+
         for (int i = 0; i < 1; i++)
         {
             try
             {
+                Scanner in = new Scanner(System.in);
+                System.out.print("Enter Integer Value for 'n' :: ");
                 n = in.nextInt();
             } catch (Exception e)
             {
@@ -22,12 +23,15 @@ public class Main extends MagicSquareCreator {
                 i--;
             }
         }
-        System.out.print("Enter Integer Value for 'm' :: ");
+
         int m = 0;
+
         for (int i = 0; i < 1; i++)
         {
             try
             {
+                Scanner in = new Scanner(System.in);
+                System.out.print("Enter Integer Value for 'm' :: ");
                 m = in.nextInt();
             } catch (Exception e)
             {
